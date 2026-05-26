eb7c5ba3e0ea75688333663b5314b685386ff14b
TNgan24 <146104057+TNgan24@users.noreply.github.com>
2026-05-26 11:33:46 +0700

    Search-Feature

---
+++ src/src/App.tsx
@@ -0,0 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
+
+import TopBanner from './components/index/TopBanner';
+import Header from './components/index/Header';
+import Footer from './components/index/Footer';
+import AuthPage from './pages/Auth';
+import { AuthProvider } from './contexts/AuthContext';
+
+import IndexPage from '@/pages/index';
+import PostDetailPage from '@/pages/PostDetailPage';
+import TagPage from '@/pages/TagPage';
+import SearchPage from '@/pages/SearchPage';
+
+function App() {
+  return (
+    <AuthProvider>
+      <div>
+        <TopBanner />
+        <Header />
+        <Routes>
+          <Route element={<IndexPage />} path="/" />
+          <Route element={<AuthPage />} path="/auth" />
+          <Route element={<SearchPage />} path="/search" />
+          <Route element={<PostDetailPage />} path="/post/:slug" />
+          <Route element={<TagPage />} path="/tag/:slug" />
+        </Routes>
+        <Footer />
+      </div>
+    </AuthProvider>
+  );
+}
+
+export default App;
